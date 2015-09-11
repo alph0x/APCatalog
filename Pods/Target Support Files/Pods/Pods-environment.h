1e4b2bea207d683d065f1f0f580e6a19231e6e2a
alph0x <alfredop88@gmail.com>
2015-09-11 19:20:39 -0430

    Initial Commit.
    Preparing the workspace.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CacheKit
+#define COCOAPODS_POD_AVAILABLE_CacheKit
+#define COCOAPODS_VERSION_MAJOR_CacheKit 0
+#define COCOAPODS_VERSION_MINOR_CacheKit 4
+#define COCOAPODS_VERSION_PATCH_CacheKit 0
+
+// CacheKit/Core
+#define COCOAPODS_POD_AVAILABLE_CacheKit_Core
+#define COCOAPODS_VERSION_MAJOR_CacheKit_Core 0
+#define COCOAPODS_VERSION_MINOR_CacheKit_Core 4
+#define COCOAPODS_VERSION_PATCH_CacheKit_Core 0
+
+// CacheKit/FastImages
+#define COCOAPODS_POD_AVAILABLE_CacheKit_FastImages
+#define COCOAPODS_VERSION_MAJOR_CacheKit_FastImages 0
+#define COCOAPODS_VERSION_MINOR_CacheKit_FastImages 4
+#define COCOAPODS_VERSION_PATCH_CacheKit_FastImages 0
+
+// FMDB
+#define COCOAPODS_POD_AVAILABLE_FMDB
+#define COCOAPODS_VERSION_MAJOR_FMDB 2
+#define COCOAPODS_VERSION_MINOR_FMDB 5
+#define COCOAPODS_VERSION_PATCH_FMDB 0
+
+// FMDB/common
+#define COCOAPODS_POD_AVAILABLE_FMDB_common
+#define COCOAPODS_VERSION_MAJOR_FMDB_common 2
+#define COCOAPODS_VERSION_MINOR_FMDB_common 5
+#define COCOAPODS_VERSION_PATCH_FMDB_common 0
+
+// FMDB/standard
+#define COCOAPODS_POD_AVAILABLE_FMDB_standard
+#define COCOAPODS_VERSION_MAJOR_FMDB_standard 2
+#define COCOAPODS_VERSION_MINOR_FMDB_standard 5
+#define COCOAPODS_VERSION_PATCH_FMDB_standard 0
+
